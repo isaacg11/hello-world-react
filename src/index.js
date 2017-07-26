1ab64d6bd2e2751340bd2f0aebb937fd051d21d7
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-07-26 13:23:33 -0700

    added components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Name extends React.Component {
+  render() {
+    return <span>{this.props.firstName} {this.props.lastName}</span>
+  }
+}
+
+class Connected extends React.Component {
+  render() {
+    return (
+      <div>
+        <Name firstName={this.props.firstName} lastName={this.props.lastName} /> is {this.props.status}
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Hello')
-  ),
+  <Connected firstName="Margaret" lastName="Hamilton" status="Connected" />,
   document.getElementById('root')
 );
