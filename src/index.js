6795393715f2e214f264ea393a11729620058933
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-07-26 10:11:10 -0700

    git remote add origin https://github.com/isaacg11/hello-world-react.git

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Hello')
+  ),
+  document.getElementById('root')
+);
