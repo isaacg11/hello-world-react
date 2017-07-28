f2046a2710c7a6eac6a1dbf8e9d31672086f9924
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-07-28 09:10:50 -0700

    counter works with redux

--- src/index.js
+++ src/index.js
@@ -3,26 +3,24 @@ import ReactDOM from 'react-dom';
 import { createStore } from "redux";
 
 const reducer = (state = 0, action) => {
-  // console.log(state);
-  // console.log(action);
   switch (action.type) {
     case 'INCREMENT':
-    return (state + 1);
+      return state + 1;
     case 'DECREMENT':
-    return (state - 1);
+      return state - 1;
     case 'RESTART':
-    return (state = 0);
+      return state = 0;
     default:
-    return state
+      return state;
   }
 }
 
-const Counter = ({ value, onIncrement, onDecrement, onReset }) => (
+const Counter = ({ value, onIncrement, onReset, onDecrement }) => (
   <div>
-  <h1>{value}</h1>
-  <button onClick={onIncrement}>+</button>
-  <button onClick={onReset}>Reset</button>
-  <button onClick={onDecrement}>-</button>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onReset}>Reset</button>
+    <button onClick={onDecrement}>-</button>
   </div>
 );
 
@@ -33,7 +31,7 @@ const render = () => {
     <Counter
     value={ store.getState() }
     onIncrement={ () => store.dispatch({ type: 'INCREMENT' }) }
-    onDecrement={ () => store.dispatch({ type: 'DECREMENT '}) }
+    onDecrement={ () => store.dispatch({ type: 'DECREMENT'}) }
     onReset={ ()=> store.dispatch({ type: 'RESTART'}) }
     />,
     document.getElementById('root')
