f286f4ed83ad5314ecbae18aa1b88b3bd95348aa
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-07-28 14:25:04 -0700

    fixing routing issue

--- src/index.js
+++ src/index.js
@@ -1,65 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from "redux";
-import { connect } from "react-redux";
-import { Provider } from "react-redux";
+import { render } from 'react-dom';
+import { Home, Mine, Search } from './screens';
 
-const initialState = {
-  count: 0
-}
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return { ...state, count: state.count + 1 };
-    case 'DECREMENT':
-      return { ...state, count: state.count - 1 };
-    case 'RESET':
-      return { ...state, count: state.count = 0 };
+const Index = ({ pathname }) => {
+  switch(pathname) {
+    case "/search":
+      return <Search />;
+    case "/mine":
+      return <Mine />;
     default:
-      return state;
+      return <Home />;
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    count: state.count
-  }
-}
+let pathname = window.location.pathname;
 
-function onIncrement() {
-  return { type: 'INCREMENT' }
-}
+render(<Index pathname={pathname} />, document.getElementById("root"));
 
-function onDecrement() {
-  return { type: 'DECREMENT' }
-}
-
-function onReset() {
-  return { type: 'RESET' }
-}
-
-const Counter = ({ count, onIncrement, onReset, onDecrement }) => (
-  <div>
-    <h1>{count}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onReset}>Reset</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
-
-const ConnectedCounter = connect(mapStateToProps, { onIncrement, onDecrement, onReset })(Counter)
-
-const store = createStore(reducer);
-
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-    <ConnectedCounter/>
-    </Provider>,
-    document.getElementById('root')
-  );
-};
-
-store.subscribe(render)
-render();
+ window.addEventListener("popstate", () => {
+  pathname = window.location.pathname;
+});
